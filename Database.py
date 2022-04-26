fc5f20b9da4eccfdc15c2be087a6e86b1cce97ae
vi.channnnn <54580948+vichannnnn@users.noreply.github.com>
2022-04-27 03:00:48 +0800

    new commit

---
+++ Database.py
@@ -0,0 +1,67 @@
+import sqlite3
+import asyncio
+
+conn = sqlite3.connect('info.db', timeout=5.0)
+c = conn.cursor()
+
+c.execute(
+    '''CREATE TABLE IF NOT EXISTS weapons (
+    weaponName TEXT PRIMARY KEY,
+    weaponType TEXT,
+    secondaryStats TEXT,
+    baseAttackRange TEXT,
+    secondaryRange TEXT,
+    weaponStats TEXT,
+    effectName TEXT,
+    effectDescription TEXT,
+    imageLink TEXT
+    )''')
+
+c.execute(
+    '''CREATE TABLE IF NOT EXISTS characters (
+    characterName TEXT PRIMARY KEY,
+    wikiLink TEXT,
+    imageLink TEXT,
+    rarityLevel TEXT,
+    weaponType TEXT,
+    characterVision TEXT,
+    characterStats TEXT
+    )''')
+
+c.execute(
+    '''CREATE TABLE IF NOT EXISTS artifacts (
+    setName TEXT PRIMARY KEY,
+    artifactOrigin TEXT,
+    imageLink TEXT,
+    wikiLink TEXT,
+    setEffect TEXT
+    )''')
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+class Database:
+    @staticmethod
+    def connect():
+        conn = sqlite3.connect('info.db', timeout=5.0, check_same_thread=False)
+        c = conn.cursor()
+        return c
+
+    @staticmethod
+    async def execute(statement, *args):
+        loop = asyncio.get_event_loop()
+        c = Database.connect()
+        await loop.run_in_executor(None, lambda: c.execute(statement, args))
+        c.connection.commit()
+        c.connection.close()
+
+    @staticmethod
+    def get(statement, *args):
+        c = Database.connect()
+        c.row_factory = dict_factory
+        res = c.execute(statement, args).fetchall()
+        c.connection.close()
+        return res
---
+++ main.py
@@ -0,0 +1,194 @@
+import traceback
+from flask import Flask, request
+from flask_restful import Resource, Api
+from marshmallow import Schema, fields
+from flask_cors import CORS
+from flask_caching import Cache
+from Database import Database
+from urllib.parse import unquote
+import ast
+import yaml
+
+with open("credentials.yaml", "r", encoding="utf8") as stream:
+    yaml_data = yaml.safe_load(stream)
+
+class APISchema(Schema):
+    name = fields.Str(required=False, default=None)
+    type = fields.Str(required=False, default=None)
+
+
+app = Flask(__name__)
+app.secret_key = ''
+api = Api(app)
+schema = APISchema()
+CORS(app)
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+
+def query(statement, *args):
+    data = [i for i in Database.get(statement, *args)]
+    if not data:
+        return None
+    result = [dict(row) for row in data]
+    return result
+
+
+def artifacts_parser(header: [], lst_of_lst: []):
+    lst = ast.literal_eval(lst_of_lst)
+    if not lst:
+        return None
+    new_data = zip(header, lst)
+    lst = dict(new_data)
+    return lst
+
+
+def stats_parser(header: [], lst_of_lst: []):
+    lst = ast.literal_eval(lst_of_lst)
+
+    if not lst:
+        return None
+    for n, l in enumerate(lst):
+        new_data = zip(header, l)
+        lst[n] = dict(new_data)
+
+    return lst
+
+
+class Weapon(Resource):
+    @cache.cached(timeout=604800, query_string=True)
+    def get(self):
+        try:
+            name = request.args.get('name', None)
+            weapon_type = request.args.get('type', None)
+
+            if name and weapon_type:
+                weapon_name = unquote(request.args['name']).replace('_', ' ')
+                result = query(f' SELECT * FROM weapons WHERE weaponType = ? AND weaponName = ?', request.args['type'],
+                               weapon_name)
+                lst = result
+
+                if not lst:
+                    return {'Error': 'Name or weapon type does not exist for weapon.'}, 400
+
+                lst = result[0]['weaponStats']
+                headers = ['Level', 'Attack', 'Secondary Stats']
+                new_lst = stats_parser(headers, lst)
+                result[0]['weaponStats'] = new_lst
+                return result[0], 200
+
+            if weapon_type:
+                result = query(f' SELECT * FROM weapons WHERE weaponType = ? ', request.args['type'])
+                headers = ['Level', 'Attack', 'Secondary Stats']
+                valid_weapon_types = ["Claymores", "Swords", "Catalysts", "Bows", "Polearms"]
+                if not result:
+                    return {
+                               'Error': f'Weapon type does not exist for weapon. Valid weapon types are: {str(valid_weapon_types)}'}, 400
+
+                for dicts in result:
+                    new_lst = stats_parser(headers, dicts['weaponStats'])
+                    dicts['weaponStats'] = new_lst
+                return result, 200
+
+            if name:
+                name = unquote(request.args['name']).replace('_', ' ')
+                result = query(f" SELECT * FROM weapons WHERE weaponName = ? ", name)
+                lst = result
+
+                if not lst:
+                    return {'Error': 'Name does not exist for weapon.'}, 400
+
+                lst = result[0]['weaponStats']
+                headers = ['Level', 'Attack', 'Secondary Stats']
+                new_lst = stats_parser(headers, lst)
+                result[0]['weaponStats'] = new_lst
+                return result[0], 200
+
+            data = [i for i in Database.get('SELECT * FROM weapons ')]
+            result = [dict(row) for row in data]
+            headers = ['Level', 'Attack', 'Secondary Stats']
+
+            for dicts in result:
+                new_lst = stats_parser(headers, dicts['weaponStats'])
+                dicts['weaponStats'] = new_lst
+
+            return result, 200
+
+        except:
+            traceback.print_exc()
+
+
+class Artifact(Resource):
+    @cache.cached(timeout=604800, query_string=True)
+    def get(self):
+        try:
+            name = request.args.get('name', None)
+            if name:
+                name = unquote(request.args['name']).replace('_', ' ')
+                result = query(f" SELECT * FROM artifacts WHERE setName = ? ", name)
+                lst = result
+
+                if not lst:
+                    return {'Error': 'Name does not exist for artifact.'}, 400
+
+                lst = result[0]['setEffect']
+                headers = ['2 Set Effect', '4 Set Effect']
+                new_lst = artifacts_parser(headers, lst) if lst else None
+                result[0]['setEffect'] = new_lst
+                return result[0], 200
+
+            data = [i for i in Database.get('SELECT * FROM artifacts ')]
+            result = [dict(row) for row in data]
+            headers = ['2 Set Effect', '4 Set Effect']
+
+            for dicts in result:
+                new_lst = artifacts_parser(headers, dicts['setEffect']) if dicts['setEffect'] else None
+                dicts['setEffect'] = new_lst
+
+            return result, 200
+
+        except:
+            traceback.print_exc()
+
+
+class Character(Resource):
+    @cache.cached(timeout=604800, query_string=True)
+    def get(self):
+        try:
+            name = request.args.get('name', None)
+            if name:
+                name = unquote(request.args['name']).replace('_', ' ')
+                result = query(f" SELECT * FROM characters WHERE characterName = ? ", name)
+                lst = result
+
+                if not lst:
+                    return {'Error': 'Name does not exist for character.'}, 400
+
+                lst = result[0]['characterStats']
+                headers = ['Level', 'Attack', 'Secondary Stats']
+                new_lst = stats_parser(headers, lst)
+                result[0]['characterStats'] = new_lst
+                return result[0], 200
+
+            data = [i for i in Database.get('SELECT * FROM characters ')]
+            result = [dict(row) for row in data]
+            headers = ['Level', 'Attack', 'Secondary Stats']
+
+            for dicts in result:
+                new_lst = stats_parser(headers, dicts['characterStats'])
+                dicts['characterStats'] = new_lst
+
+            return result, 200
+
+        except:
+            traceback.print_exc()
+
+
+api.add_resource(Weapon, '/api/weapons', endpoint='weapons')
+api.add_resource(Character, '/api/characters', endpoint='characters')
+api.add_resource(Artifact, '/api/artifacts', endpoint='artifacts')
+
+if __name__ == '__main__':
+    certs = ('cert.pem', 'key.pem')
+    # app.run()
+    app.run(host=yaml_data['HOST'], port=yaml_data['PORT'], ssl_context=certs)
